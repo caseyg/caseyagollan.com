c181d5f95f3e3f746e75aa08d3795c82d61adb92
Casey Gollan <hello@caseyagollan.com>
2025-12-01 23:00:18 -0500

    fix build warnings (#58)

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,8 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	build: {
+		chunkSizeWarningLimit: 1000 // Increase limit for Three.js (3D library visualization)
+	}
 });
